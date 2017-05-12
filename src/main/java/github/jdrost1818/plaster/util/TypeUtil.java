a182a734c3b0e36ceb1a40dff0ecf82291b66025
Jake Drost <JDrost1818@gmail.com>
2017-05-11 22:14:22 -0500

    Filled out rest of TypeUtil and tested it

--- src/main/java/github/jdrost1818/plaster/util/TypeUtil.java
+++ src/main/java/github/jdrost1818/plaster/util/TypeUtil.java
@@ -1,13 +1,17 @@
 package github.jdrost1818.plaster.util;
 
-import com.google.common.base.CaseFormat;
+import com.google.common.collect.Lists;
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @UtilityClass
 public class TypeUtil {
+
+    private static final String WITH_DELIMITER = "((?<=%1$s)|(?=%1$s))";
 
     /**
      * Converts potentially weirdly-formed, yet valid type declarations into something
@@ -15,29 +19,85 @@ public class TypeUtil {
      *
      * Example:
      *
-     *      "Map<  String,Integer>" -> "Map<String, Integer>"
+     *      "map<  string,Integer>" -> "Map<String, Integer>"
      *
      * @param typeString
      *          string to format
      * @return the formatted string
      */
     public static String normalizeTypeString(String typeString) {
-        return typeString
-                .replaceAll(" ", "")
-                .replaceAll(",", ", ");
+        if (StringUtils.isBlank(typeString)) {
+            return "";
+        }
+
+        String noSpacesString = typeString.replaceAll(" ", "");
+
+        return Arrays.stream(noSpacesString.split(String.format(WITH_DELIMITER, "<")))
+                .map(s -> s.split(String.format(WITH_DELIMITER, ",")))
+                .flatMap(Arrays::stream)
+                .map(s -> s.split(String.format(WITH_DELIMITER, ">")))
+                .flatMap(Arrays::stream)
+                .map(TypeUtil::normalizeSingleType)
+                .collect(Collectors.joining()).replaceAll(",", ", ");
     }
 
+    /**
+     * This transforms the text given into a variable name that follows standard Java practices
+     *
+     * Example:
+     *
+     *      someVar -> someVar
+     *      some_var -> someVar
+     *      SomeVar -> someVar
+     *      Some_Var -> someVar
+     *
+     * @param variableName
+     *          name to change
+     * @return java variable name
+     */
     public static String normalizeVariableName(String variableName) {
-        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, variableName.toLowerCase());
+        if (StringUtils.isBlank(variableName)) {
+            return "";
+        }
+
+        String varName = Arrays.stream(variableName.split("[_ ]"))
+                .map(StringUtils::capitalize)
+                .collect(Collectors.joining(""));
+
+        return StringUtils.uncapitalize(varName).replaceAll(" ", "");
     }
 
+    /**
+     * Takes in a type string and returns the types found withing
+     *
+     * Example:
+     *
+     *  Map<String, Integer> -> [Map, String, Integer]
+     *
+     * @param typeString
+     *          string from which to extract type strings
+     * @return the list of types found within the given string
+     */
     public static List<String> splitToIndividualTypes(String typeString) {
+        if (StringUtils.isBlank(typeString)) {
+            return Lists.newArrayList();
+        }
+
         // Map<String, List<Something>> -> Map String List Something
         return Arrays.asList(typeString
                 .replaceAll("[<>,]", " ")
                 .replaceAll(" +", " ")
                 .trim()
                 .split(" "));
+    }
+
+    private static String normalizeSingleType(String type) {
+        if (StringUtils.isBlank(type)) {
+            return "";
+        } else if (type.matches("[<,>]")) {
+            return type;
+        }
+        return StringUtils.capitalize(normalizeVariableName(type));
     }
 
 }
--- src/test/java/github/jdrost1818/plaster/util/TypeUtilTest.java
+++ src/test/java/github/jdrost1818/plaster/util/TypeUtilTest.java
@@ -1,10 +1,92 @@
 package github.jdrost1818.plaster.util;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class TypeUtilTest {
+
     @Test
-    public void normalizeVariableName() throws Exception {
+    public void normalizeTypeString_null() throws Exception {
+        assertThat(TypeUtil.normalizeTypeString(null), equalTo(""));
     }
 
+    @Test
+    public void normalizeTypeString_empty() throws Exception {
+        assertThat(TypeUtil.normalizeTypeString(""), equalTo(""));
+    }
+
+    @Test
+    public void normalizeTypeString_standard_var() throws Exception {
+        assertThat(TypeUtil.normalizeTypeString("Integer"), equalTo("Integer"));
+    }
+
+    @Test
+    public void normalizeTypeString_messedUp() throws Exception {
+        String input = "map<customClass,    List<  other_class>>";
+        String expected = "Map<CustomClass, List<OtherClass>>";
+
+        assertThat(TypeUtil.normalizeTypeString(input), equalTo(expected));
+    }
+
+    @Test
+    public void normalizeVariableName_null() throws Exception {
+        assertThat(TypeUtil.normalizeVariableName(null), equalTo(""));
+    }
+
+    @Test
+    public void normalizeVariableName_empty() throws Exception {
+        assertThat(TypeUtil.normalizeVariableName(""), equalTo(""));
+    }
+
+    @Test
+    public void normalizeVariableName_already_good() throws Exception {
+        assertThat(TypeUtil.normalizeVariableName("alreadyGoodVarName"), equalTo("alreadyGoodVarName"));
+    }
+
+    @Test
+    public void normalizeVariableName_bad_capitalization() throws Exception {
+        assertThat(TypeUtil.normalizeVariableName("BadCapitalization"), equalTo("badCapitalization"));
+    }
+
+    @Test
+    public void normalizeVariableName_snake_case() throws Exception {
+        assertThat(TypeUtil.normalizeVariableName("should_be_camel_case"), equalTo("shouldBeCamelCase"));
+    }
+
+    @Test
+    public void normalizeVariableName_bad_spacing() throws Exception {
+        assertThat(TypeUtil.normalizeVariableName("should be camel case"), equalTo("shouldBeCamelCase"));
+    }
+
+    @Test
+    public void normalizeVariableName_all_bad_things() throws Exception {
+        assertThat(TypeUtil.normalizeVariableName("Should be_camelCase"), equalTo("shouldBeCamelCase"));
+    }
+
+    @Test
+    public void splitToIndividualTypes_null() throws Exception {
+        assertThat(TypeUtil.splitToIndividualTypes(null), equalTo(Lists.newArrayList()));
+    }
+
+    @Test
+    public void splitToIndividualTypes_empty() throws Exception {
+        assertThat(TypeUtil.splitToIndividualTypes(""), equalTo(Lists.newArrayList()));
+    }
+
+    @Test
+    public void splitToIndividualTypes_standard_var() throws Exception {
+        assertThat(TypeUtil.splitToIndividualTypes("Integer"), equalTo(Lists.newArrayList("Integer")));
+    }
+
+    @Test
+    public void splitToIndividualTypes_multiple() throws Exception {
+        List<String> expected = Lists.newArrayList("Map", "List", "Integer", "String");
+
+        assertThat(TypeUtil.splitToIndividualTypes("Map<List<Integer>, String>"), equalTo(expected));
+    }
 }
