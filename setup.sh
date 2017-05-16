fca95b7122b785cac988069aaae9cd8e09ab3193
Jake Drost <JDrost1818@gmail.com>
2017-05-16 13:21:59 -0500

    Started the installation scripts

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+uninstall_plaster() {
+    sudo rm /usr/bin/plaster;
+    sudo rm -rf /usr/share/plaster;
+}
+
+install_plaster() {
+    echo "Building project"
+    mvn clean install -DskipTests=true -q;
+
+    sudo mkdir /usr/share/plaster;
+    sudo mkdir /usr/share/plaster/lib;
+
+    plaster_file_name=""
+    cd target/;
+    for file_name in plaster*.jar
+    do
+        plaster_file_name=${file_name};
+    done
+
+    sudo cp ${plaster_file_name} /usr/share/plaster/;
+    sudo cp lib/* /usr/share/plaster/lib/;
+
+    sudo touch /usr/bin/plaster;
+    sudo chmod 777 /usr/bin/plaster;
+    sudo chmod 777 /usr/share/plaster;
+    sudo chmod 777 /usr/share/plaster/${plaster_file_name};
+
+    sudo echo "#!/bin/sh" >> /usr/bin/plaster
+    sudo echo "" >> /usr/bin/plaster
+    sudo echo "java -jar /usr/share/plaster/${plaster_file_name}" >> /usr/bin/plaster
+
+    cd ..
+}
+
+uninstall="n"
+install="y"
+if test -e /usr/bin/plaster
+then
+    read -p "Plaster already installed, would you like to reinstall (y/n) " uninstall
+fi
+
+uninstall_plaster
+install_plaster
--- src/main/java/github/jdrost1818/plaster/Plaster.java
+++ src/main/java/github/jdrost1818/plaster/Plaster.java
@@ -33,6 +33,7 @@ public class Plaster {
     private static FieldService fieldService = ServiceProvider.getFieldService();
 
     public static void main(String[] args) {
+        System.out.println("Current working directory: " + System.getProperty("user.dir"));
         ArgumentParser parser = ArgParseUtil.getArgParser();
         Namespace parsedArgs;
         try {
