2661e3257bb6ada9645e5e09b11e776728fad671
Jake Drost <JDrost1818@gmail.com>
2017-07-06 12:41:20 -0500

    Can now uninstall/install plaster on Mac

--- setup.sh
+++ setup.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 uninstall_plaster() {
+    bin_location="$1"
+    share_location="$2"
+
     echo "Uninstalling current installation of plaster"
-    sudo rm /usr/bin/plaster;
-    sudo rm -rf /usr/share/plaster;
+    sudo rm ${bin_location}/plaster;
+    sudo rm -rf ${share_location}/plaster;
 }
 
-install_linux() {
+install_plaster() {
+    bin_location="$1"
+    share_location="$2"
+
     echo "Building project"
     mvn clean install -DskipTests=true -q;
 
     echo "Installing plaster"
-    sudo mkdir /usr/share/plaster;
-    sudo mkdir /usr/share/plaster/lib;
+    sudo mkdir ${share_location}/plaster;
+    sudo mkdir ${share_location}/plaster/lib;
 
     plaster_file_name=""
     cd target/;
@@ -21,49 +27,52 @@ install_linux() {
         plaster_file_name=${file_name};
     done
 
-    sudo cp ${plaster_file_name} /usr/share/plaster/;
-    sudo cp lib/* /usr/share/plaster/lib/;
+    sudo cp ${plaster_file_name} ${share_location}/plaster/;
+    sudo cp lib/* ${share_location}/plaster/lib/;
 
-    sudo touch /usr/bin/plaster;
-    sudo chmod 777 /usr/bin/plaster;
-    sudo chmod 777 /usr/share/plaster;
-    sudo chmod 777 /usr/share/plaster/${plaster_file_name};
+    sudo touch ${bin_location}/plaster;
+    sudo chmod 777 ${bin_location}/plaster;
+    sudo chmod 777 ${share_location}/plaster;
+    sudo chmod 777 ${share_location}/plaster/${plaster_file_name};
 
-    echo "#!/bin/sh" >> /usr/bin/plaster
-    echo "" >> /usr/bin/plaster
-    echo "java -jar /usr/share/plaster/${plaster_file_name} \$@" >> /usr/bin/plaster
+    echo "#!/bin/sh" >> ${bin_location}/plaster
+    echo "" >> ${bin_location}/plaster
+    echo "java -jar ${share_location}/plaster/${plaster_file_name} \$@" >> ${bin_location}/plaster
 
     cd ..
 
     echo "Successfully installed plaster"
 }
 
-install_mac() {
-    echo "Installing for mac"
-}
+bin_location=""
+share_location=""
 
-install_plaster() {
-    device="$(uname -s)"
-    if [ "$device" == "Darwin" ]
-    then
-        install_mac;
-    elif [ "$device" == "Linux" ]
-    then
-        install_linux;
-    else
-        echo "Unsupported command line tool"
-    fi
-}
+device="$(uname -s)"
+if [ "$device" == "Darwin" ]
+then
+    echo "Mac OS detected"
+    bin_location="/usr/local/bin"
+    share_location="/usr/local/share"
+elif [ "$device" == "Linux" ]
+then
+    echo "Linux OS detected"
+    bin_location="/usr/bin"
+    share_location="/usr/share"
+else
+    echo "Unsupported command line tool"
+    exit
+fi
+
 reinstall="first_install"
-if test -e /usr/bin/plaster
+if test -e ${bin_location}/plaster
 then
     read -p "Plaster already installed, would you like to reinstall (y/n) " reinstall
 fi
 
 case ${reinstall} in
-    [Yy]* ) uninstall_plaster; break;;
+    [Yy]* ) uninstall_plaster ${bin_location} ${share_location}; break;;
     first_install ) break;;
     * ) exit;;
 esac
 
-install_plaster
+install_plaster ${bin_location} ${share_location}
