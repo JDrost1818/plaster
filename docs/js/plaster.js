4ddd16decb3875a2ea63faec68d7d640b753723e
Jake Drost <JDrost1818@gmail.com>
2017-07-17 20:29:32 -0500

    Added support for landing on a selected tab

--- docs/js/plaster.js
+++ docs/js/plaster.js
@@ -1,13 +1,54 @@
 $( document ).ready(function() {
 
-    var iframe = $("#documentation-iframe")[0];
+    const iframe = $("#documentation-iframe")[0];
 
     $(".doc-link-entry a").each(function(index, elem) {
-        var link = "documentation/" + $(elem).data("rel") + ".html";
-
         $(elem).click(function (){
-            $(iframe).attr('src', link);
+            changeIFrame(iframe, $(elem).data("rel"));
         });
-    })
+    });
+
+    var selectedUrl = getUrlParameter("selected");
+    if (selectedUrl) {
+        changeIFrame(iframe, selectedUrl);
+    }
 
 });
+
+const supportedPages = ["general", "mode", "configuration", "scope", "class-info"];
+
+/**
+ * Changes the iframe's source to the one provided as long as it is
+ * a supported page.
+ * @param iframe
+ *          iframe for which to change source
+ * @param relLink
+ *          the link to which to change
+ */
+function changeIFrame(iframe, relLink) {
+    if ($.inArray(relLink, supportedPages) === -1) {
+        relLink = "general";
+    }
+    $(iframe).attr('src', "documentation/" + relLink + ".html");
+}
+
+/**
+ * Useful utility function that allows a programmer to extract a
+ * url parameter from the current url listing.
+ *
+ * Found here: https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
+ */
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
