d73adffc20795717025fdfca0af8ab61ae087771
Jake Drost <JDrost1818@gmail.com>
2017-07-19 11:51:00 -0500

    Now updates README.md automatically

--- src/main/java/github/jdrost1818/plaster/dev/UpdateVersions.java
+++ src/main/java/github/jdrost1818/plaster/dev/UpdateVersions.java
@@ -2,6 +2,8 @@ package github.jdrost1818.plaster.dev;
 
 import com.github.zafarkhaja.semver.Version;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -10,44 +12,75 @@ import java.util.stream.Collectors;
 
 public class UpdateVersions {
 
-    public static void main(String[] args) throws IOException {
-        updatePom();
-        updateReadMe();
+    private enum UpdateType {
+        MAJOR(Version::incrementMajorVersion),
+        MINOR(Version::incrementMinorVersion),
+        PATCH(Version::incrementPatchVersion);
+
+        VersionUpdater updater;
+        UpdateType(VersionUpdater versionUpdater) {
+            this.updater = versionUpdater;
+        }
     }
 
-    private static void updatePom() throws IOException {
+    private static final String VERSION_REGEX = "\\d\\.\\d\\.\\d";
+
+    public static void main(String[] args) throws IOException {
+        UpdateType updateType;
+        try {
+            updateType = UpdateType.valueOf(StringUtils.upperCase(args[0]));
+        } catch (Exception e) {
+            System.out.println("Must provide an update type: ['major', 'minor', 'patch']");
+            return;
+        }
+
+        updatePom(updateType);
+        updateReadMe(updateType);
+    }
+
+    private static void updatePom(UpdateType updateType) throws IOException {
         String before = "<version>";
         String after = "</version><!--PROJECT_ID-->";
         String filename = "pom.xml";
 
-        updateSemVer(filename, before, after);
+        updateSemVer(filename, before, after, updateType);
     }
 
-    private static void updateReadMe() throws IOException {
-        String before = "IDK";
-        String after = "IDK";
+    private static void updateReadMe(UpdateType updateType) throws IOException {
+        String before = "\\[!\\[Version for JDrost1818/plaster]\\(https://img\\.shields\\.io/SemVer/";
+        String after = "\\.png\\)]\\(\\)";
         String filename = "README.md";
+
+        updateSemVer(filename, before, after, updateType);
     }
 
     @SuppressWarnings("unchecked")
-    private static void updateSemVer(String filename, String before, String after) throws IOException {
+    private static void updateSemVer(String filename, String before, String after, UpdateType updateType) throws IOException {
         File file = Paths.get(System.getProperty("user.dir"), filename).toFile();
 
-
         List<String> convertedLines = (List<String>) FileUtils.readLines(file).stream()
-                .map(Object::toString)
                 .map(line -> {
                     String lineStr = line.toString();
-                    if (lineStr.trim().matches(before + "\\d\\.\\d\\.\\d" + after)) {
-                        Version version = Version.valueOf(lineStr.trim().replace(before, "").replace(after, ""));
-                        Version newVersion = version.incrementMinorVersion();
+                    if (lineStr.trim().matches(before + VERSION_REGEX + after)) {
+                        Version version = Version.valueOf(
+                                lineStr.trim()
+                                        .replaceAll(before, "")
+                                        .replaceAll(after, ""));
 
-                        return lineStr.replaceAll("\\d\\.\\d\\.\\d", newVersion.getNormalVersion());
+                        Version newVersion = updateType.updater.updateVersion(version);
+
+                        return lineStr.replaceAll(VERSION_REGEX, newVersion.getNormalVersion());
                     }
                     return line;
                 }).collect(Collectors.toList());
 
         FileUtils.writeLines(file, convertedLines);
+    }
+
+    private interface VersionUpdater {
+
+        public Version updateVersion(Version versionToUpdate);
+
     }
 
 }
