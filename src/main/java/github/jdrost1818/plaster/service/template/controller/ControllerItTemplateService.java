3097f7b292e4dcb48ca0aecabb24f8a08185e0c5
Jake Drost <JDrost1818@gmail.com>
2017-08-22 20:00:43 -0500

    Prepped both IT and ITDoc template services

--- src/main/java/github/jdrost1818/plaster/service/template/controller/ControllerItTemplateService.java
+++ src/main/java/github/jdrost1818/plaster/service/template/controller/ControllerItTemplateService.java
@@ -7,25 +7,24 @@ import github.jdrost1818.plaster.service.template.TemplateService;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
-public class ControllerITDocService extends TemplateService {
+public class ControllerItTemplateService extends TemplateService {
 
-    public ControllerITDocService(ConfigurationService configurationService) {
-        super(TemplateType.CONTROLLER_IT_DOC, configurationService);
+    public ControllerItTemplateService(ConfigurationService configurationService) {
+        super(TemplateType.CONTROLLER_IT, configurationService);
     }
 
     @Override
     protected JtwigModel addCustomInformation(JtwigModel model, FileInformation fileInformation) {
         model = super.addId(model, fileInformation);
         model = super.addTypeField(model, fileInformation.getClassName(), TemplateType.CONTROLLER);
-        model = super.addTypeField(model, fileInformation.getClassName(), TemplateType.CONTROLLER_IT_DOC);
+        model = super.addTypeField(model, fileInformation.getClassName(), TemplateType.CONTROLLER_IT);
         model = super.addTypeField(model, fileInformation.getClassName(), TemplateType.MODEL);
         model = super.addTypeField(model, fileInformation.getClassName(), TemplateType.SERVICE);
 
-        return model;
-    }
+        return model;    }
 
     @Override
     protected JtwigTemplate getTemplate() {
-        return JtwigTemplate.classpathTemplate("template/controller/controllerITDoc.twig");
+        return JtwigTemplate.classpathTemplate("template/controller/controllerIT.twig");
     }
 }
